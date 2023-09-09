3ab8dee58ad0b67aa8efc51e14bd68049e92a842
samuel-1 <samuelifeoluwa914@gmail.com>
2023-09-09 23:40:16 +0100

    'changes'

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 
-    var daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-    var currentDate = new Date();
-    var currentDay = daysOfWeek[currentDate.getUTCDay()];
-    document.querySelector('[data-testid="currentDayOfTheWeek"]').textContent = 'Current Day: ' + currentDay;
+const currentDate = new Date();
+const currentDayOfWeek = currentDate.getUTCDay();
 
-    var currentUTCTime = currentDate.getTime();
-    document.querySelector('[data-testid="currentUTCTime"]').textContent = 'Current UTC Time: ' + currentUTCTime;
+
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+
+const currentUTCHours = currentDate.getUTCHours();
+const currentUTCMinutes = currentDate.getUTCMinutes();
+const currentUTCSeconds = currentDate.getUTCSeconds();
+
+
+function formatTimeUnit(unit) {
+  return unit < 10 ? `0${unit}` : unit;
+}
+
+const formattedTime = `${formatTimeUnit(currentUTCHours)}:${formatTimeUnit(currentUTCMinutes)}:${formatTimeUnit(currentUTCSeconds)}`;
+
+
+const dayOfWeekText = `Today is ${daysOfWeek[currentDayOfWeek]}`;
+const utcTimeText = `Current UTC Time: ${formattedTime}`;
+ 
+document.getElementById('dayofWeek').textContent = dayofWeekText; 
+document.getElementById('utcTIme').textContent = utcTimeText; 
